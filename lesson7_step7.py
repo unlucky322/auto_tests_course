c7749e168957b056874963e958283cb05f2faaec
Nikita <147027521+unlucky322@users.noreply.github.com>
2026-09-14 20:02:54 +0300

    Кликаем по checkboxes и radiobuttons. Поиск сокровища с помощью get_attribute

---
+++ lesson7_step7.py
@@ -0,0 +1,33 @@
+import time
+import math
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+def calc(x):
+  return str(math.log(abs(12*math.sin(int(x)))))
+
+link  = "https://suninjuly.github.io/get_attribute.html"
+
+try:
+    browser = webdriver.Chrome()
+    browser.get(link)
+
+    x_element = browser.find_element(By.CSS_SELECTOR, "#treasure")
+    x = x_element.get_attribute("valuex")
+    y = calc(x)
+
+    input1 = browser.find_element(By.CSS_SELECTOR, "#answer")
+    input1.send_keys(y)
+
+    option1 = browser.find_element(By.CSS_SELECTOR, "#robotCheckbox")
+    option1.click()
+
+    option2 = browser.find_element(By.CSS_SELECTOR, "#robotsRule")
+    option2.click()
+
+    button = browser.find_element(By.CSS_SELECTOR, "button.btn")
+    button.click()
+
+    time.sleep(3)
+finally:
+    browser.quit()
